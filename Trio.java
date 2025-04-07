e4ec9953ff72b63ba6b502f6f78da27080aca7a2
bLeohr <benjamin.leohr@stu.daviess.kyschools.us>
2025-04-07 14:29:01 +0000

    thisIsCrapPleaseDontBeatMeImTired

--- Trio.java
+++ Trio.java
@@ -1,1 +1,30 @@
 //Write your answer here
+
+
+public class Trio implements MenuItem
+{
+
+    static double sand = 0;
+    static double sal = 0;
+    static double dri = 0;
+
+    public static void main(String []args)
+
+    public Trio(double sandwich, double salad, double drink)
+    {
+        sand = sandwich.getPrice();
+        sal = salad.getPrice();
+        dri = drink.getPrice();
+    }
+
+    public static String getName(String item)
+    {
+        return item;
+    }
+
+
+    public static double getPrice(double price)
+    {
+        return price;
+    }
+}
